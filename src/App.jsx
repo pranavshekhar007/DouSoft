5e047c00ed204f73602430d6a34f6ff61e13ddbd
Pranav Shekhar <pranavshekhar00@gmail.com>
2025-05-01 17:36:46 +0530

    added a footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import HeroSection from './components/HeroSection'
 import FeatureSection from './components/FeatureSection'
 import Workflow from './components/Workflow'
 import Pricing from './components/Pricing'
+import Testimonials from './components/Testimonials'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,6 +19,8 @@ function App() {
         <FeatureSection />
         <Workflow />
         <Pricing />
+        <Testimonials />
+        <Footer />
         </div>
       </div>
     </>
